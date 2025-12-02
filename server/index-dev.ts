3a3b2fd2d228121fff150d263bfc7612bddea835
Aoi Ueki <129098557+Aoi-U@users.noreply.github.com>
2025-12-01 20:43:36 -0700

    removed duplicate client folder

--- server/index-dev.ts
+++ server/index-dev.ts
@@ -39,7 +39,6 @@ export async function setupVite(app: Express, server: Server) {
       const clientTemplate = path.resolve(
         import.meta.dirname,
         "..",
-        "client",
         "index.html",
       );
 
